c282788181b235b0075e46fe941d35f51ddab2b1
robincpc <chienpinchen@gmail.com>
2019-03-14 14:43:33 -0700

    Add grc_client node to communicate GRC controller through simple string command.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # nexgrc_experimental
 NexGRC Experimental
+
+nexgrc_clinet node provide ros service to talk with socket (and send command to GRC Controller)
+```
+nexgrc_clien/StringCommand
+string command
+string parameters
+---
+int32 error_code
+string response
+```
+
+## How to Build [Indigo]:
+Go to your ROS workspace directory, then
+``` bash
+    # remove old compiled file (if exist) and rebuild
+    catkin clean
+    catkin build
+    source devel/setup.bash
+```
+
+## Usage:
+Currently, Only use `StringCommand` to communicate with GRC controller through tcpip.
+``` bash
+    # After build.
+    roslaunch nexgrc_client start.launch
+    # Then, client node will wait for connecting NexGRC controller
+```
+
+For testing, open another terminal
+``` bash
+    # Go to you ROS worksace, and source nexgrc_client package
+    source devel/setup.bash
+    # ask robot to do ptp move.
+    rosservice call /grc_telnet 'ptp' ' -35 130 -15 45 90 -115'
+    # get joint position
+    rosservice call /grc_telnet 'get joint position' ''
+    # get flange frame
+    rosservice call /grc_telnet 'get flange frame' ''
+    # check if motion finish
+    rosservice call /grc_telnet 'get state' ''
+    # ask robot to stop while executing ptp command
+    rosservice call /grc_telnet 'halt' ''
+```
+
---
+++ nexgrc_client/scripts/grc_client.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2019 NexCOBOT Inc.
+# Author: Robin Chen
+#
+# -----------------------------------------------------------------------------
+# This node manages the connection between ROS and NexGRC controller through a TCP/IP connection
+# The service provided accepts string commands with parameters and returns the produced answer from the robot.
+# -----------------------------------------------------------------------------
+import rospy
+import socket
+import thread
+import time
+from nexgrc_client.srv import *
+
+# Default  global parameters
+TCP_IP = "192.168.88.100"
+TCP_PORT = 1688
+SERVICE_NAME = 'grc_telnet'
+
+# Create tcpip socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lock = thread.allocate_lock() 	# Lock for tcp communication. Only one command can be sent at the same time
+
+# Connect with NexGRC Controller
+def connect():
+    while not rospy.is_shutdown():
+        try:
+            rospy.logwarn('Connecting to ' + TCP_IP + ':' + str(TCP_PORT))
+            sock.connect((TCP_IP, TCP_PORT))
+            rospy.logwarn('Connected to ' + TCP_IP + ':' + str(TCP_PORT))
+            break
+        except:
+            # Wait and reconnect
+            time.sleep(1)
+
+# Flush line through the socket.
+def flush(line):
+    while not rospy.is_shutdown():
+        try:
+            sock.send(line)
+            return sock.recv(1024)
+        except:
+            connect()
+
+# ROS service: transforms the received command into string
+def send_command(req):
+    with lock:
+        response = StringCommandResponse()      # ROS StringCommandResponse service response
+        response.error_code = 0
+
+        rospy.logdebug('Send: ' + req.command + ' : ' + req.parameters + '\n')
+        response.response = flush(req.command + ' : ' + req.parameters + '\n')
+        rospy.logdebug('Received: ' + response.response)
+
+        if response.response == 'error':
+            response.error_code = 1
+        return response
+
+
+# Main Function
+if __name__ == '__main__':
+    # Start node
+    rospy.init_node('grc_client')
+
+    # TODO: Load variables from config.yaml
+
+    connect()
+
+    srv = rospy.Service(SERVICE_NAME, StringCommand, send_command)
+
+    rospy.spin()
+
